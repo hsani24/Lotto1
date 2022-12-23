03c4f1b8f669b555308b613041ae84927b3b11f3
hsani24 <110380167+hsani24@users.noreply.github.com>
2022-12-23 09:38:24 +0100

    Initial Commit

---
+++ Lotto1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lotto1
+//
+//  Created by Sani I on 23.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
